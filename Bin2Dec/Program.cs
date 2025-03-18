d45a84ad1ba57480279c5ca93508c9cd251790dc
majua <majua@POSIR15-MAJUJI>
2025-03-18 19:26:04 -0300

    Add project files.

---
+++ Bin2Dec/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Input binary value: ");
+string numStr = Console.ReadLine();
+
+int num = int.Parse(numStr);
+string[] numbers = {"2", "3","4", "5", "6", "7", "8", "9"};
+
+if (numStr.Length < 1 || numStr.Length > 8)
+{
+    Console.WriteLine("Invalid binary number.");
+    Environment.ExitCode = 1;
+}
+else if (numbers.Any(numStr.Contains)){
+    Console.WriteLine("Invalid binary number.");
+    Environment.ExitCode = 1;
+}
